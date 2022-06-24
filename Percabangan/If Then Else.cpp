0d1788c1cb0a94148957ff428118f7dce2c5315a
Coder2026 <108107693+Coder2026@users.noreply.github.com>
2022-06-24 13:41:58 +0700

    Add files via upload

---
+++ Percabangan/If Then Else.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int N;
+	cin>>N;
+	
+	if(N>0){
+		cout<<"positif\n";
+	}else if(N<0){
+		cout<<"negatif\n";
+	}else{
+		cout<<"nol\n";
+	}
+	return 0;
+}
